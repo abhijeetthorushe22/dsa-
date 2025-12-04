fd4bda9ca8b41c98d71d96cc1ff561ee606b1e64
Abhijeet Thorushe <thorusheabhijeet@gmail.com>
2025-12-05 00:29:15 +0530

    Time: 7 ms (44.76%) | Memory: 25.5 MB (91.87%) - LeetSync

--- linkedlist/189-rotate-array/rotate-array.py
+++ linkedlist/189-rotate-array/rotate-array.py
@@ -1,18 +1,19 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         n = len(nums)
-        k = k%n
-        arr = []
-        for i in range(n-k,n):
-            arr.append(nums[i])
-        
-        for i in range(0,n-k):
-            arr.append(nums[i])
-        
-        for i in range(n):
-            nums[i]= arr[i]
-        return nums
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+        k = k % n
+
+        def reverse(i,j):
+            
+
+            while i < j :
+                nums[i],nums[j]=nums[j],nums[i]
+                i+=1
+                j-=1
+    
+        reverse(0,n-1)
+        reverse(0,k-1)
+        reverse(k,n-1)
+
+      
         

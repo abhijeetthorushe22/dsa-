80c7bc3b1fef05ee160c6d988c8a66e9898748a6
Abhijeet Thorushe <thorusheabhijeet@gmail.com>
2025-12-07 23:46:55 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (49.27%) - LeetSync

---
+++ linkedlist/66-plus-one/plus-one.py
@@ -0,0 +1,7 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        x = "".join(map(str,digits))
+        x = str(int(x) + 1)
+        x = [int(ch) for ch in x]
+        return x
+        

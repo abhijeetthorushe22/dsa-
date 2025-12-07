5146f8393b08c1b112e554ee5b9d3d60b6184cca
Abhijeet Thorushe <thorusheabhijeet@gmail.com>
2025-12-07 12:04:22 +0530

    Time: 4003 ms (5.99%) | Memory: 19.6 MB (76.52%) - LeetSync

--- linkedlist/136-single-number/single-number.py
+++ linkedlist/136-single-number/single-number.py
@@ -1,6 +1,8 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-       
-        
-        return  reduce(xor, nums)
+        count = 0
+        for num in nums:
+            count = nums.count(num)
+            if count == 1:
+                return num
         

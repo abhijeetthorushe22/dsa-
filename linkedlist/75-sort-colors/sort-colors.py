7bb9830510604e74af88365461f5410bc4aaf94e
Abhijeet Thorushe <thorusheabhijeet@gmail.com>
2025-12-12 00:04:49 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (33.34%) - LeetSync

--- linkedlist/75-sort-colors/sort-colors.py
+++ linkedlist/75-sort-colors/sort-colors.py
@@ -1,6 +1,22 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        return nums.sort()
+        ans = []
+        for i in range(len(nums)):
+            if nums[i] == 0 :
+                ans.append(nums[i])
+                i+=1
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                ans.append(nums[i])
+                i+=1
+        for i in range(len(nums)):
+            if nums[i] == 2 :
+                ans.append(nums[i])
+                i+=1
+        
+        for i in range(len(ans)):
+            nums[i]=ans[i]
+        return nums
         """
         Do not return anything, modify nums in-place instead.
         """

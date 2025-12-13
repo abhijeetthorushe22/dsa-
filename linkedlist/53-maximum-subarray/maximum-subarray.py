e625a5a459ab4bdd1b1af0bd65856ea7f3855169
Abhijeet Thorushe <thorusheabhijeet@gmail.com>
2025-12-13 16:39:12 +0530

    Time: 92 ms (26.12%) | Memory: 32.5 MB (51.20%) - LeetSync

--- linkedlist/53-maximum-subarray/maximum-subarray.py
+++ linkedlist/53-maximum-subarray/maximum-subarray.py
@@ -2,8 +2,7 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         maxsum = cursum = nums[0]
         for num in nums[1:]:
-            cursum = max(num,cursum+num)
+
+            cursum = max(cursum+num,num)
             maxsum = max(cursum,maxsum)
-    
         return maxsum
-        

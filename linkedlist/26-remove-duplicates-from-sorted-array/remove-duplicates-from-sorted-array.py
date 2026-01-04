654b78dc189909e084f3444b90ef94dcffed8adb
Abhijeet Thorushe <thorusheabhijeet@gmail.com>
2026-01-04 13:30:37 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (93.67%) - LeetSync

--- linkedlist/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ linkedlist/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -4,10 +4,7 @@ class Solution:
         for j in range(1,len(nums)):
             if nums[j]!=nums[j-1]:
                 nums[i]=nums[j]
-                i=i+1
-              
+                i+=1
         
         return i
-        
-
         

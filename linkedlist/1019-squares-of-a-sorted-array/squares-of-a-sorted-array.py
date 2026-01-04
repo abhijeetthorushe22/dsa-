69650995fac710d1730a39de02bef866b8c3891c
Abhijeet Thorushe <thorusheabhijeet@gmail.com>
2026-01-05 00:42:23 +0530

    Time: 11 ms (53.01%) | Memory: 18.5 MB (99.38%) - LeetSync

---
+++ linkedlist/1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        i = 2
+        for j in range(len(nums)):
+            nums[j]=nums[j]**2
+            j+=1
+        nums.sort()
+        return nums
+        
